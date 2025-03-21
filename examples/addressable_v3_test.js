33f25a4a41553cafb6393bba59dc0765d086dd14
Martijn Poppen <m.poppen@ziggo.nl>
2025-03-21 16:41:03 +0100

    OPT: custom control

--- examples/addressable_v3_test.js
+++ examples/addressable_v3_test.js
@@ -1,41 +1,73 @@
-const { ControlAddressable, AddressableColorStopMode, SingleSegmentsMode, AddressableCustomModeStep } = require("../");
+const {
+  ControlAddressable,
+  AddressableColorStopMode,
+  SingleSegmentsMode,
+  AddressableCustomModeStep,
+} = require("../");
 
-const controller = new ControlAddressable("192.168.0.65", { log_add_received: true, command_timeout: 2000 });
+const controller = new ControlAddressable("192.168.178.254", {
+  log_add_received: true,
+  command_timeout: null,
+});
 
 // const steps = [
 //     (new AddressableCustomModeStep())
+//         .setEffect(5)
+//         .setForegroundColor(0,0,255)
+//         .setBackgroundColor(0,255,0),
+//         (new AddressableCustomModeStep())
+//         .setEffect(8)
+//         .setForegroundColor(255,0,0)
+//         .setBackgroundColor(0,255,0),
+//         (new AddressableCustomModeStep())
 //         .setEffect(2)
 //         .setForegroundColor(255,0,0)
 //         .setBackgroundColor(0,255,0)
 // ];
 
-// const mode = new SingleSegmentsMode(50);
-// mode.setPointColor(5, 255, 0, 0);
-// mode.setPointColor(25, 255, 0, 0);
-
-const mode = new AddressableColorStopMode(50);
-mode.addColorStop(10, 255, 0, 0);
-mode.addColorStop(40, 0, 0, 255);
+// const mode = new SingleSegmentsMode(160);
+// mode.setPointColor(0, 255, 0, 0);
+// mode.setPointColor(80, 255, 0, 0);
+// mode.setEffect('running_water')
 
 async function main() {
-    // await controller.turnOn();
+  const config = await controller.queryDeviceConfig();
+  console.log(await controller.queryDeviceConfig());
+//   const mode = new AddressableColorStopMode(config.pixel_count)
+//     .setEffect("jump")
+//     .setSpeed(50)
+//     .addColorStop(0, 255, 0, 0)
+//     .addColorStop(40, 0, 0, 255)
+//     .addColorStop(80, 255, 0, 0)
+//     .addColorStop(120, 0, 0, 255)
+//     .addColorStop(159, 255, 0, 0)
+const mode = new AddressableColorStopMode(config.pixel_count)
+	.addColorStop(0, 255, 0, 0)
+	.addColorStop(80, 0, 0, 255)
+    .setEffect("running_water")
+    .setSpeed(10)
+    
 
-    // await controller.setIAPattern(20, 50);
+//   await controller.turnOff();
 
-    // await controller.setFixedMode({
-    //     effect: 1,
-    //     foreground: { green: 255, },
-    //     // background: { red: 255 },
-    //     speed: 100,
-    //     // reversed: true,
-    // });
+  // await controller.setIAPattern(20, 50);
 
-    // await controller.setRbmMode(2);
+  console.log(await controller.setColor(0, 10, 0));
 
-    await controller.setMultiColorMode(mode);
-    // await controller.setCustomMode(steps);
-    // console.log(await controller.queryDeviceConfig());
-    // console.log(await controller.setColor(0, 255, 0));
+//   await controller.setRbmMode(1, 100, 10);
+//   await controller.turnOn();
+  await controller.setMultiColorMode(mode);
+
+
+  //
+  console.log(await controller.queryDeviceConfig());
+  console.log(await controller.queryState());
+  
 }
 
-main().then(() => {}, err => console.error(err));
+main()
+  .then(
+    () => {},
+    (err) => console.error(err)
+  )
+  .catch((err) => console.log(err));
